e35ef265bc31985e3847fc11f8297291acb4a6be
William Pursell <bill.pursell@gmail.com>
2009-01-01 21:18:49 +0000

    Add gnulib call in autogen.sh.

---
+++ autogen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This script will bootstrap a git working directory.
+# You'll need to have gnulib available, with its
+# toplevel dir in your PATH.
+gnulib-tool --import getloadavg
+autoreconf --install --verbose
