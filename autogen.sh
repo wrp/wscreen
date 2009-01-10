2b7d807b9071583848dc46dfdd3f272c941d838c
William Pursell <bill.pursell@gmail.com>
2009-01-10 08:13:19 +0000

    Abort autogen.sh if gnulib-tool fails.
    If gnulib-tool is not in the user's path, autoreconf
    should not be run.  This script should be made more
    robust, but this is okay for now.

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,5 @@
 # This script will bootstrap a git working directory.
 # You'll need to have gnulib available, with its
 # toplevel dir in your PATH.
-gnulib-tool --update
+gnulib-tool --update &&
 autoreconf --install --verbose
