faeeb17b15f86c25736198f7df3133aab6f296d8
William Pursell <bill.pursell@gmail.com>
2009-07-13 08:01:36 -0500

    Add instructions on obtaining gnulib in autogen.sh comment

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,11 @@
 # This script will bootstrap a git working directory.
 # You'll need to have gnulib available, with its
 # toplevel dir in your PATH.
+# To obtain gnulib, you might try:
+#
+# cd $HOME
+# git clone git://git.savannah.gnu.org/gnulib
+# export PATH=$PATH:$HOME/gnulib
+
 gnulib-tool --update &&
 autoreconf --install --verbose
